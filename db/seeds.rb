ba116080193122833fcb3ad7bc1fb36ba5573b9d
rtoledo_dev <rodrigo@rtoledo.inf.br>
2024-05-21 11:59:03 -0300

    Update attributes in Current model, remove Hostel and Manager models and related migrations, and update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
-hostel = Hostel.create(name: 'Hostel Example')
-Manager.create(hostel: hostel, email: 'manager@example.com', password: 'password', password_confirmation: 'password')
 Admin.create(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 user = User.create(name: Faker::Name.name_with_middle, email: 'user@example.com', uid: SecureRandom.uuid,
                    password: 'password', password_confirmation: 'password')
 2.times do
-  event = Event.create(hostel: hostel, title: Faker::Esport.event, location: Faker::Address.full_address,
+  event = Event.create(title: Faker::Esport.event, location: Faker::Address.full_address,
                        description: Faker::Lorem.paragraph, scheduled_at: Time.current + 10.days, confirmation_ends_at: Date.today + 10.days, active: true)
   2.times do
     user = User.create(name: Faker::Name.name_with_middle, email: Faker::Internet.email,
